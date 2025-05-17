efa9cfa6380be4411de9be5ea83a635c0d8a4205
Kayuan165 <kayuandiasoli@gmail.com>
2025-05-16 23:27:20 -0300

    feat: Cria a movimentação para o player

--- game/game.go
+++ game/game.go
@@ -13,18 +13,22 @@ func NewGame() *Game {
 	}
 }
 
+//roda em 60 fps
 // reponsavel por atualizar a logica do jogo
+// 60 vezes por segundo
 func (g *Game) Update() error {
+	g.player.Update()
 	return nil
 }
 
 // responsavel por desenhar o jogo
+//roda 60 vezes por segundo
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.player.Draw(screen)
 
 }
 
 // responsavel por definir o tamanho da tela
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return outsideWidth, outsideHeight
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
--- game/player.go
+++ game/player.go
@@ -7,18 +7,36 @@ import (
 )
 
 type Player struct {
-	image *ebiten.Image
+	image    *ebiten.Image
+	position Vector
 }
 
 func NewPlayer() *Player {
 
 	image := assets.PlayerSprite
+	bounds := image.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+
+	position := Vector{
+		x: (screenWidth / 2) - halfW,
+		y: 500,
+	}
+
 	return &Player{
-		image: image,
+		image:    image,
+		position: position,
 	}
 }
 
 func (p *Player) Update() {
+
+	speed := 6.0
+
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		p.position.x -= speed
+	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		p.position.x += speed
+	}
 
 }
 
@@ -26,7 +44,7 @@ func (p *Player) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 
 	//posição x e y que a  imagem vai ser desenhada
-	op.GeoM.Translate(100, 100)
+	op.GeoM.Translate(p.position.x, p.position.y)
 	//desenha a imagem na tela
 	screen.DrawImage(p.image, op)
 }
