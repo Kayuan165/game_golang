83a8986e8569b7917afe81d45edd837f8c3a4ed2
Kayuan165 <kayuandiasoli@gmail.com>
2025-05-16 22:52:30 -0300

    feat: cria estrutura inicial para o jogo

--- main.go
+++ main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import "github.com/hajimehoshi/ebiten/v2"
+
+type Game struct {
+}
+
+//reponsavel por atualizar a logica do jogo
+func (g *Game) Update() error {
+	return nil
+}
+
+//responsavel por desenhar o jogo
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+//responsavel por definir o tamanho da tela
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+
+	g := &Game{}
+
+	err := ebiten.RunGame(g)
+
+	if err != nil {
+		panic(err)
+	}
+}
